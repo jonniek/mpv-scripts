dfe17177c76fabf68d3f6f4417503b9851a15c61
donmaiq <kayhty@windowslive.com>
2017-02-13 22:08:54 +0200

    mupd

--- cycleaudiodevice.lua
+++ cycleaudiodevice.lua
@@ -7,13 +7,27 @@ local devices = {
   { ['name'] = 'pulse/alsa_output.pci-0000_06_00.0.analog-stereo', ['nicename'] = 'speaker' },
 }
 
-function setdevice(forceindex)
-  if forceindex then index = tonumber(forceindex) end
+function setdevice()
   mp.set_property("audio-device", devices[index]['name'])
   mp.osd_message("Audio device set: "..devices[index]['nicename'])
 end
 
-mp.register_script_message("setaudiodevice", setdevice)
+function setdevicebymessage(message)
+  if message then
+    local match = false
+    for deviceindex in ipairs(devices) do
+      if devices[deviceindex]['nicename']:lower() == message:lower() then
+        index = deviceindex
+        match = true
+        break
+      end
+    end
+    if not match then index = tonumber(message) end
+    setdevice()
+  end
+end
+
+mp.register_script_message("setaudiodevice", setdevicebymessage)
 mp.add_key_binding("ctrl+A", "cycleaudiodevice", function()
   if not #devices or not devices then return end
   if #devices ~= index then
