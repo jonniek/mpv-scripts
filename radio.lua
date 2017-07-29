2b241ba3c6be2a44ff10e69aed8614efab7dc335
donmaiq <kayhty@windowslive.com>
2017-07-29 23:44:42 +0300

    refactored radio

--- radio.lua
+++ radio.lua
@@ -1,50 +1,55 @@
---Usage: 1. Create a internet-radio playlist in m3u format, see example playlist on github repo
---       2. Run [ mpv --idle ] and use the shortcut R to launch the radio
---          Optionally you can run [ mpv --script-opts=radiostart=true] to start without input
+--[[
+1. Create a internet-radio playlist in m3u format
+2. Run [ mpv --idle ] and use the shortcut R to launch the radio
+   Optionally you can run [ mpv --no-video --idle=once --script-opts=radio-autostart=yes ] to open on startup
+   note: volume in http profile will override this script volume
+]]--
 
-local settings ={
-    vol = '35',                                         --radio start volume
-    savepath = '/custom/radiosongs',                   --path to saved songs textfile
-    playlist = "https://stream.r-a-d.io/main.mp3"     --radio playlist with your channels
+local settings = {
+    autostart = false, --setting to start on mpv start, requires --idle=once or --idle=yes
+    vol = 70, --radio start volume
+    savepath = "/home/anon/radiosongs", --path to saved songs file
+    playlist = "/home/anon/radio.m3u" --radio playlist with your channels
 }
+
+require 'mp.options'
+read_options(settings, "radio")
+local msg = require 'mp.msg'
 
 local radio_playing = false
 
-function volume()
-    mp.command("set volume "..settings.vol)
-    mp.unobserve_property(volume)
-end
-
 function radiotoggle()
-    if radio_playing == false then
-        mp.observe_property("volume", "number", volume)
-        mp.commandv("loadfile", settings.playlist, "replace")
-        radio_playing=true
-    else
-        mp.command("stop")
-        radio_playing=false
-    end
+  if radio_playing == false then
+    mp.commandv("loadfile", settings.playlist, "replace")
+    mp.set_property("volume", settings.vol)
+    radio_playing=true
+  else
+    mp.command("stop")
+    radio_playing=false
+  end
 end
 
 function radiomark()
-    local songname = mp.get_property('media-title')
-    if songname then
-        local file = io.open(settings.savepath, "a+")
-        if file then 
-            file:write(songname, "\n")
-            print('Saved: '..songname)
-            file:close()
-        else
-            print("Failed to open song savepath, check permissions")
-        end
-    else 
-        print("No songname available") 
+  if not radio_playing then return end
+  local songname = mp.get_property('media-title')
+  local filename = mp.get_property('filename')
+  if songname ~= filename then
+    local file = io.open(settings.savepath, "a+")
+    if file then 
+      file:write(songname, "\n")
+      msg.info('Saved: '..songname)
+      file:close()
+    else
+      msg.error("Failed to open savepath, check path and permissions")
     end
+  else
+    msg.error("No title available") 
+  end
 end
 
 mp.add_key_binding('r', 'mark-song', radiomark)
 mp.add_key_binding('R', 'radio-toggle', radiotoggle)
 
-if mp.get_opt("radiostart") then
-    radiotoggle()
+if settings.autostart then
+  radiotoggle()
 end
